f412a4aec5b3ad8c865db593b49149eeb9cb1ece
Eduardo Coelho <eduardo@educoelho.com>
2012-08-10 15:17:15 -0300

    added support for NSLocalizedString() in 'Last Updated:' text

--- EGOTableViewPullRefresh/Classes/View/EGORefreshTableHeaderView.m
+++ EGOTableViewPullRefresh/Classes/View/EGORefreshTableHeaderView.m
@@ -117,7 +117,9 @@
 		[dateFormatter setDateStyle:NSDateFormatterShortStyle];
 		[dateFormatter setTimeStyle:NSDateFormatterShortStyle];
 
-		_lastUpdatedLabel.text = [NSString stringWithFormat:@"Last Updated: %@", [dateFormatter stringFromDate:date]];
+		_lastUpdatedLabel.text = [NSString stringWithFormat:@"%@ %@",
+                                  NSLocalizedString(@"Last Updated:", Nil),
+                                  [dateFormatter stringFromDate:date]];
 		[[NSUserDefaults standardUserDefaults] setObject:_lastUpdatedLabel.text forKey:@"EGORefreshTableView_LastRefresh"];
 		[[NSUserDefaults standardUserDefaults] synchronize];
 		
